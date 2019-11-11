ab63daa7b769dd643c383031389111351dfbb973
Doug Liu <dougdliu@gmail.com>
2019-11-10 17:47:14 -0700

    1. Added styles.css to fix the display of the page 2. Fixed app.component.css 3. Fixed dashboard.component.html

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -17,7 +16,7 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
